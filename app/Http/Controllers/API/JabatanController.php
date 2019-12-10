f158e66700ce22bffdb88173f63161e7aa0c8ae9
muhammadasadbastomi <tomycules@gmail.com>
2019-12-10 23:31:47 +0800

    add get method jabatan controler

--- app/Http/Controllers/API/JabatanController.php
+++ app/Http/Controllers/API/JabatanController.php
@@ -10,5 +10,15 @@ use Illuminate\Support\Facades\Redis;
 
 class JabatanController extends APIController
 {
-    //
+    public function get(){
+        $jabatan = json_decode(redis::get("jabatan::all"));
+        if (!$jabatan) {
+            $jabatan = jabatan::with('golongan')->get();
+            if (!$jabatan) {
+                return $this->returnController("error", "failed get jabatan data");
+            }
+            Redis::set("jabatan:all", $jabatan);
+        }
+        return $this->returnController("ok", $jabatan);
+    }
 }
