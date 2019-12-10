be9da579d96ccec5bc55d983fbfa0af6a51de59f
muhammadasadbastomi <tomycules@gmail.com>
2019-12-10 23:30:26 +0800

    initialize require library for jabatan controller

---
+++ app/Http/Controllers/API/JabatanController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Jabatan;
+use HCrypt;
+use Illuminate\Support\Facades\Redis;
+
+class JabatanController extends APIController
+{
+    //
+}
