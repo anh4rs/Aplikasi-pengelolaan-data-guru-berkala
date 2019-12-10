847bb6beb9998be2f58e85ac42a1e0b8576cf0d8
muhammadasadbastomi <tomycules@gmail.com>
2019-12-10 22:56:59 +0800

    add get method golongan

--- app/Http/Controllers/API/GolonganController.php
+++ app/Http/Controllers/API/GolonganController.php
@@ -8,5 +8,15 @@ use Illuminate\Support\Facades\Redis;
 
 class GolonganController extends APIController
 {
-    //
+    public function get(){
+        $golongan = json_decode(redis::get("golongan::all"));
+        if (!$golongan) {
+            $golongan = golongan::all();
+            if (!$golongan) {
+                return $this->returnController("error", "failed get golongan data");
+            }
+            Redis::set("golongan:all", $golongan);
+        }
+        return $this->returnController("ok", $golongan);
+        }
 }
