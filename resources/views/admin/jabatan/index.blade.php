4e6b67ecfc27ecef8117c474dd04e491785c16a8
tri angga utama <trianggategarutama@gmail.com>
2019-12-10 09:11:36 +0800

    tambah script crud jabatan

--- resources/views/admin/jabatan/index.blade.php
+++ resources/views/admin/jabatan/index.blade.php
@@ -33,23 +33,6 @@
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                   <td>K218</td>
-                    <td>
-                      Bendahara Perencanaan Keudangan
-                    </td>
-                    <td>
-                    <button class="btn btn-sm  btn-outline-success" type="button">
-	                    <span class="btn-inner--icon">detail</span>
-                    </button>
-                    <button class="btn btn-sm  btn-outline-primary" type="button">
-	                    <span class="btn-inner--icon">edit</span>
-                    </button>
-                    <button class="btn btn-sm btn-outline-danger" type="button">
-	                    <span class="btn-inner--icon">hapus</span>
-                    </button>
-                    </td>
-                  </tr>
                 </tbody>
               </table>
             </div>
@@ -71,12 +54,12 @@
             <div class="modal-body">
                 <form action="" method="post">
                     <div class="form-group">
-                        <label for="golongan">Kode Jabatan</label>
-                        <input type="text" class="form-control" name="kd_jabatan" id="golongan">
+                        <label for="jabatan">Kode Jabatan</label>
+                        <input type="text" class="form-control" name="kode_jabatan" id="kode_jabatan">
                     </div>
                     <div class="form-group">
-                        <label for="golongan">Nama Jabatan</label>
-                        <input type="text" class="form-control" name="nama_jabatan" id="keterangan">
+                        <label for="jabatan">Nama Jabatan</label>
+                        <input type="text" class="form-control" name="nama" id="nama">
                     </div>
                 </form> 
             </div>   
@@ -90,14 +73,142 @@
 </div>
 @endsection
 @section('script')
+
 <script>
+function hapus(uuid, nama){
+    var csrf_token=$('meta[name="csrf_token"]').attr('content');
+    Swal.fire({
+                title: 'apa anda yakin?',
+                text: " Menghapus  Data jabatan " + nama,
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'hapus data',
+                cancelButtonText: 'batal',
+                reverseButtons: true
+            }).then((result) => {
+                if (result.value) {
+                    $.ajax({
+                        url : "{{ url('/api/jabatan')}}" + '/' + uuid,
+                        type : "POST",
+                        data : {'_method' : 'DELETE', '_token' :csrf_token},
+                        success: function (response) {
+                            Swal.fire({
+                            position: 'top-end',
+                            icon: 'success',
+                            title: 'Data Berhasil Dihapus',
+                            showConfirmButton: false,
+                            timer: 1500
+                        })
+                    $('#datatable').DataTable().ajax.reload(null, false);
+                },
+            })
+            } else if (result.dismiss === swal.DismissReason.cancel) {
+                Swal.fire(
+                'Dibatalkan',
+                'data batal dihapus',
+                'error'
+                )
+            }
+        })
+    }
+    $('#tambah').click(function(){
+        $('.modal-title').text('Tambah Data');
+        $('#kode_jabatan').val('');
+        $('#nama').val('');  
+        $('#btn-form').text('Simpan Data');
+        $('#mediumModal').modal('show');
+    })
+    function edit(uuid){
+        $.ajax({
+            type: "GET",
+            url: "{{ url('/api/jabatan')}}" + '/' + uuid,
+            beforeSend: false,
+            success : function(returnData) {
+                $('.modal-title').text('Edit Data');
+                $('#id').val(returnData.data.uuid);
+                $('#kode_jabatan').val(returnData.data.kode_jabatan);
+                $('#nama').val(returnData.data.nama);
+                $('#btn-form').text('Ubah Data');
+                $('#mediumModal').modal('show');
+            }
+        })
+    }
 $(document).ready(function() {
-    $('#datatable').DataTable();
-} );
-$('#tambah').click(function(){
-    $('.modal-title').text('Tambah Data');
-    $('#btn-form').text('Simpan Data');
-    $('#mediumModal').modal('show');
-});
-</script>
+    $('#datatable').DataTable( {
+        responsive: true,
+        processing: true,
+        serverSide: false,
+        searching: true,
+        ajax: {
+            "type": "GET",
+            "url": "{{route('API.jabatan.get')}}",
+            "dataSrc": "data",
+            "contentType": "application/json; charset=utf-8",
+            "dataType": "json",
+            "processData": true
+        },
+        columns: [
+            {"data": "kode_jabatan"},
+            {"data": "nama"},
+            {data: null , render : function ( data, type, row, meta ) {
+                var uuid = row.uuid;
+                var nama = row.nama;
+                return type === 'display'  ?
+                '<button onClick="edit(\''+uuid+'\')" class="btn btn-sm btn-outline-primary" data-toggle="modal" data-target="#editmodal"><i class="ti-pencil"></i></button> <button onClick="hapus(\'' + uuid + '\',\'' + nama + '\')" class="btn btn-sm btn-outline-danger" > <i class="ti-trash"></i></button>':
+            data;
+            }}
+        ]
+    });
+    $("form").submit(function (e) {
+        e.preventDefault()
+        var form = $('#modal-body form');
+        if($('.modal-title').text() == 'Edit Data'){
+            var url = '{{route("API.jabatan.update", '')}}'
+            var id = $('#id').val();
+            $.ajax({
+                url: url+'/'+id,
+                type: "put",
+                data: $(this).serialize(),
+                success: function (response) {
+                    form.trigger('reset');
+                    $('#mediumModal').modal('hide');
+                    $('#datatable').DataTable().ajax.reload();
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: 'Data Berhasil Tersimpan',
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                },
+                error:function(response){
+                    console.log(response);
+                }
+            })
+        }else{
+            $.ajax({
+                url: "{{Route('API.jabatan.create')}}",
+                type: "post",
+                data: $(this).serialize(),
+                success: function (response) {
+                    form.trigger('reset');
+                    $('#mediumModal').modal('hide');
+                    $('#datatable').DataTable().ajax.reload();
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: 'Data Berhasil Disimpan',
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                },
+                error:function(response){
+                    console.log(response);
+                }
+            })
+        }
+    } );
+    } );
+    </script>
 @endsection
