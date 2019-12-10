da794c642559603f0b8529b624a534ee34f8cf36
muhammadasadbastomi <tomycules@gmail.com>
2019-12-10 22:51:41 +0800

    initialize require library golongan controler

---
+++ app/Http/Controllers/API/GolonganController.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Golongan;
+use HCrypt;
+use Illuminate\Support\Facades\Redis;
+
+class GolonganController extends APIController
+{
+    //
+}
