74d575c8482d6dcac20ba06188670bb13ca0e90b
muhammadasadbastomi <tomycules@gmail.com>
2019-12-10 23:49:14 +0800

    add fillable and hidden method on model

--- app/Sekolah.php
+++ app/Sekolah.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sekolah extends Model
 {
-    //
+    protected $fillable = [
+        'uuid','NPSN', 'status','b_pendidikan','status_pemilik', 'sk',
+        'tgl_sk','sk_izin', 'tgl_sk_izin',
+    ];
+
+    protected $hidden = [
+        'id'
+    ];
 }
