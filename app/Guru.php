a30f47cba74ce39332b2c1949b22da866f04d8ff
muhammadasadbastomi <tomycules@gmail.com>
2019-12-11 09:55:00 +0800

    add fillable and hidden method

--- app/Guru.php
+++ app/Guru.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Guru extends Model
 {
-    //
+    protected $fillable = [
+        'user_id','sekolah_id','jabatan_id','uuid','telepon','tempat_lahir', 'tgl_lahir','alamat'
+    ];
+
+    protected $hidden = [
+        'id','user_id','sekolah_id','jabatan_id'
+    ];
 }
