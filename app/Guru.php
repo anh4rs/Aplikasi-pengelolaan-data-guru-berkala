ef0614fde9faf7ab2586979be754db31c020e214
muhammadasadbastomi <tomycules@gmail.com>
2019-12-11 21:44:10 +0800

    fix fillable guru

--- app/Guru.php
+++ app/Guru.php
@@ -7,10 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Guru extends Model
 {
     protected $fillable = [
-        'user_id','sekolah_id','jabatan_id','mata_pelajaran_id','uuid','telepon','tempat_lahir', 'tgl_lahir','alamat'
+        'golongan_id','sekolah_id','jabatan_id','mata_pelajaran_id','uuid','telepon','tempat_lahir', 'tgl_lahir','alamat','status'
     ];
 
     protected $hidden = [
-        'id','user_id','sekolah_id','jabatan_id','mata_pelajaran_id'
+        'id','golongan_id','sekolah_id','jabatan_id','mata_pelajaran_id'
     ];
 }
