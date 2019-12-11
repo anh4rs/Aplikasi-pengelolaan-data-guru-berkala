8adf3bf98443abd8356141625d295540aad97b6b
muhammadasadbastomi <tomycules@gmail.com>
2019-12-11 10:05:09 +0800

    add foreign mapel id

--- app/Guru.php
+++ app/Guru.php
@@ -7,10 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Guru extends Model
 {
     protected $fillable = [
-        'user_id','sekolah_id','jabatan_id','uuid','telepon','tempat_lahir', 'tgl_lahir','alamat'
+        'user_id','sekolah_id','jabatan_id','mata_pelajaran_id','uuid','telepon','tempat_lahir', 'tgl_lahir','alamat'
     ];
 
     protected $hidden = [
-        'id','user_id','sekolah_id','jabatan_id'
+        'id','user_id','sekolah_id','jabatan_id','mata_pelajaran_id'
     ];
 }
