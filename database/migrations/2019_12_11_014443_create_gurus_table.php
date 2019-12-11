233306a95b51ebf6cb31cdbeb2c8ca311bcb3fd1
muhammadasadbastomi <tomycules@gmail.com>
2019-12-11 21:42:13 +0800

    fix field table guru

--- database/migrations/2019_12_11_014443_create_gurus_table.php
+++ database/migrations/2019_12_11_014443_create_gurus_table.php
@@ -15,8 +15,8 @@ class CreateGurusTable extends Migration
     {
         Schema::create('gurus', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('sekolah_id');
+            $table->unsignedBigInteger('golongan_id');
             $table->unsignedBigInteger('jabatan_id');
             $table->unsignedBigInteger('mata_pelajaran_id')->nullable();
             $table->text('uuid')->nullable();
@@ -25,11 +25,12 @@ class CreateGurusTable extends Migration
             $table->string('tempat_lahir')->length(100);
             $table->date('tgl_lahir')->length(100);
             $table->text('alamat');
+            $table->string('status')->length(25);
             $table->timestamps();
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('sekolah_id')->references('id')->on('sekolahs')->onDelete('cascade');
+            $table->foreign('golongan_id')->references('id')->on('golongans')->onDelete('cascade');
             $table->foreign('jabatan_id')->references('id')->on('jabatans')->onDelete('cascade');
-            $table->foreign('mata_pelajaran_id')->references('id')->on('mata_pelajarans')->onDelete('cascade');
+            $table->foreign('mata_pelajaran_id')->references('id')->on('mata_pelajarans')->onDelete('set null');
         });
     }
 
