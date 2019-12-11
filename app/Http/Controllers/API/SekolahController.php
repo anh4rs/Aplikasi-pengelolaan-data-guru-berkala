f4e8067758b4c4361648137c400a61826e104ee1
muhammadasadbastomi <tomycules@gmail.com>
2019-12-11 09:21:50 +0800

    add delete method on sekolah

--- app/Http/Controllers/API/SekolahController.php
+++ app/Http/Controllers/API/SekolahController.php
@@ -66,10 +66,30 @@ class SekolahController extends APIController
             return $this->returnController("error", "failed find data sekolah");
         
         }
-        $golongan->fill($req->all())->save();
+        $sekolah->fill($req->all())->save();
 
         Redis::del("sekolah:all");
         Redis::set("sekolah:$id", $sekolah);
         return $this->returnController("ok", $sekolah);
+    }
+
+    public function delete($uuid){
+        $id = HCrypt::decrypt($uuid);
+        if (!$id) {
+            return $this->returnController("error", "failed decrypt uuid");
+        }
+        $sekolah = sekolah::findOrFail($id);
+        if (!$sekolah) {
+            return $this->returnController("error", "failed find data sekolah");
+        }
+        // Need to check realational
+        // If there relation to other data, return error with message, this data has relation to other table(s)
+        $delete = $sekolah->delete();
+        if (!$delete) {
+            return $this->returnController("error", "failed delete data sekolah");
+        }
+        Redis::del("sekolah:all");
+        Redis::del("sekolah:$id");
+        return $this->returnController("ok", "success delete data sekolah");
     }
 }
