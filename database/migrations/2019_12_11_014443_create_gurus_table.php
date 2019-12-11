e1672aa2dc867b08f7f692f8db71169cce115442
muhammadasadbastomi <tomycules@gmail.com>
2019-12-11 10:03:44 +0800

    add field on gurus migration table

--- database/migrations/2019_12_11_014443_create_gurus_table.php
+++ database/migrations/2019_12_11_014443_create_gurus_table.php
@@ -15,7 +15,21 @@ class CreateGurusTable extends Migration
     {
         Schema::create('gurus', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('sekolah_id');
+            $table->unsignedBigInteger('jabatan_id');
+            $table->unsignedBigInteger('mata_pelajaran_id')->nullable();
+            $table->text('uuid')->nullable();
+            $table->string('NIP')->length(50);
+            $table->string('telepon')->length(13);
+            $table->string('tempat_lahir')->length(100);
+            $table->date('tgl_lahir')->length(100);
+            $table->text('alamat');
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('sekolah_id')->references('id')->on('sekolahs')->onDelete('cascade');
+            $table->foreign('jabatan_id')->references('id')->on('jabatans')->onDelete('cascade');
+            $table->foreign('mata_pelajaran_id')->references('id')->on('mata_pelajarans')->onDelete('cascade');
         });
     }
 
