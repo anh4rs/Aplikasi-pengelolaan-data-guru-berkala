dc236572a1527ae7a84f4b01967904d3a8dc9284
muhammadasadbastomi <tomycules@gmail.com>
2019-12-11 21:49:12 +0800

    add get method guru

--- app/Http/Controllers/API/GuruController.php
+++ app/Http/Controllers/API/GuruController.php
@@ -10,5 +10,15 @@ use HCrypt;
 
 class GuruController extends APIController
 {
-    //
+    public function get(){
+        $guru = json_decode(redis::get("guru::all"));
+        if (!$guru) {
+            $guru = guru::all();
+            if (!$guru) {
+                return $this->returnController("error", "failed get guru data");
+            }
+            Redis::set("guru:all", $guru);
+        }
+        return $this->returnController("ok", $guru);
+    }
 }
