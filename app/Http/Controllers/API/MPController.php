c7bee10b2f01c4921623a95b4c57c8c25e8193c4
muhammadasadbastomi <tomycules@gmail.com>
2019-12-11 09:39:22 +0800

    add find method mapel

--- app/Http/Controllers/API/MPController.php
+++ app/Http/Controllers/API/MPController.php
@@ -22,4 +22,20 @@ class MPController extends APIController
         return $this->returnController("ok", $mata_pelajaran);
     }
 
+    public function find($uuid){
+        $id = HCrypt::decrypt($uuid);
+        if (!$id) {
+            return $this->returnController("error", "failed decrypt uuid");
+        }
+        $mata_pelajaran = Redis::get("mata_pelajaran:$id");
+        if (!$mata_pelajaran) {
+            $mata_pelajaran = mata_pelajaran::find($id);
+            if (!$mata_pelajaran){
+                return $this->returnController("error", "failed find data mata_pelajaran");
+            }
+            Redis::set("mata_pelajaran:$id", $mata_pelajaran);
+        }
+        return $this->returnController("ok", $mata_pelajaran);
+    }
+
 }
