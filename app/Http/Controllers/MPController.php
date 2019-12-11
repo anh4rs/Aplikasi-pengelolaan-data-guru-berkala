d434a3cef36e5b06b941a893617f201b5783c51b
muhammadasadbastomi <tomycules@gmail.com>
2019-12-11 09:34:12 +0800

    add get method mapel

--- app/Http/Controllers/MPController.php
+++ app/Http/Controllers/MPController.php
@@ -10,5 +10,16 @@ use Illuminate\Support\Facades\Redis;
 
 class MPController extends APIController
 {
-    //
+    public function get(){
+        $mata_pelajaran = json_decode(redis::get("mata_pelajaran::all"));
+        if (!$mata_pelajaran) {
+            $mata_pelajaran = mata_pelajaran::all();
+            if (!$mata_pelajaran) {
+                return $this->returnController("error", "failed get mata_pelajaran data");
+            }
+            Redis::set("mata_pelajaran:all", $mata_pelajaran);
+        }
+        return $this->returnController("ok", $mata_pelajaran);
+    }
+
 }
