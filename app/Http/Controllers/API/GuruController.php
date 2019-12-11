39d586011b4fa9e8e1f1b5142e6a759836323f85
muhammadasadbastomi <tomycules@gmail.com>
2019-12-11 21:46:36 +0800

    initialize require library gurucontroller

---
+++ app/Http/Controllers/API/GuruController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Support\Facades\Redis;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Golongan;
+use HCrypt;
+
+class GuruController extends APIController
+{
+    //
+}
