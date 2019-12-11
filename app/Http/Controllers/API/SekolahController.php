dedaf14aa013834fbe7fb5fa131e884a2cb49685
muhammadasadbastomi <tomycules@gmail.com>
2019-12-11 08:50:58 +0800

    add get method sekolah

--- app/Http/Controllers/API/SekolahController.php
+++ app/Http/Controllers/API/SekolahController.php
@@ -10,5 +10,15 @@ use Illuminate\Support\Facades\Redis;
 
 class SekolahController extends APIController
 {
-    //
+    public function get(){
+        $sekolah = json_decode(redis::get("sekolah::all"));
+        if (!$sekolah) {
+            $sekolah = sekolah::with('bidang')->get();
+            if (!$sekolah) {
+                return $this->returnController("error", "failed get sekolah data");
+            }
+            Redis::set("sekolah:all", $sekolah);
+        }
+        return $this->returnController("ok", $sekolah);
+    }
 }
