6b75a7d682c4ec77806bde36318eba01c63ff559
muhammadasadbastomi <tomycules@gmail.com>
2019-12-11 09:26:36 +0800

    add fillable and hidden methood

--- app/Mata_pelajaran.php
+++ app/Mata_pelajaran.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Mata_pelajaran extends Model
 {
-    //
+    protected $fillable = [
+        'uuid','kode_mp', 'nama'
+    ];
+
+    protected $hidden = [
+        'id'
+    ];
 }
