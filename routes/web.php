7995598234f13101dfba9c19f5f8c775d8c19753
tri angga utama <trianggategarutama@gmail.com>
2020-01-10 20:25:57 +0800

    tambah halaman berita all

--- routes/web.php
+++ routes/web.php
@@ -122,9 +122,10 @@ Route::get('/pejabatStruktural/keseluruhanCetak', 'adminController@pejabatStrukt
 
 //halaman data Mata Pelajaran
 Route::get('/berita/index', 'adminController@beritaIndex')->name('beritaIndex');
-Route::get('/berita/detail/{id}', 'adminController@beritaDetail')->name('beritaDetail');
 Route::get('/berita/Cetak', 'adminController@beritaCetak')->name('beritaCetak');
 });
 
 Auth::routes();
 Route::get('/', 'adminController@depan')->name('depan');
+Route::get('/berita/all', 'adminController@beritaAll')->name('beritaAll');
+Route::get('/berita/detail/{id}', 'adminController@beritaDetail')->name('beritaDetail');
