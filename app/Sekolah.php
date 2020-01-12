4fbf24741f0fd4132c8cedf169f9265ed4068575
muhammadasadbastomi <tomycules@gmail.com>
2020-01-12 12:32:59 +0800

    fix column user_id not found

--- app/Sekolah.php
+++ app/Sekolah.php
@@ -7,12 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Sekolah extends Model
 {
     protected $fillable = [
-        'uuid','nama','NPSN', 'status_sekolah','b_pendidikan','status_pemilik', 'sk',
+        'uuid','user_id','nama','NPSN', 'status_sekolah','b_pendidikan','status_pemilik', 'sk',
         'tgl_sk','sk_izin', 'tgl_sk_izin',
     ];
 
     protected $hidden = [
-        'id'
+        'id','user_id'
     ];
 
     public function user(){
