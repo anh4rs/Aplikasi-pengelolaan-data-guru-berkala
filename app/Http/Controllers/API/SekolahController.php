2ab56e08d8f90a11898e5f151e4c41cc459da429
muhammadasadbastomi <tomycules@gmail.com>
2020-01-12 12:16:25 +0800

    fix method delete and update sekolah

--- app/Http/Controllers/API/SekolahController.php
+++ app/Http/Controllers/API/SekolahController.php
@@ -87,17 +87,55 @@ class SekolahController extends APIController
             return $this->returnController("error", "failed decrypt uuid");
         }
 
-        $sekolah = Sekolah::findOrFail($id);
+        $sekolah = sekolah::findOrFail($id);
+        $user_id = $sekolah->user_id;
+        $user = User::findOrFail($user_id);
+        if (!$user){
+                return $this->returnController("error", "failed find data sekolah");
+            }
+        if($req->foto != null){
+                $FotoExt  = $req->foto->getClientOriginalExtension();
+                $FotoName = $req->user_id.' - '.$req->name;
+                $foto   = $FotoName.'.'.$FotoExt;
+                $req->foto->move('images/user', $foto);
+                $user->foto       = $foto;
+                }else {
+                    $user->foto  = $user->foto;
+                }
+            $user->name            = $req->name;
+            $user->email    = $req->email;
+            if($req->password != null){
+                $password       = Hash::make($req->password);
+                $user->password = $password;
+            }else{
+                $user->password = $user->password;
+            }
+            'uuid','nama','NPSN', 'status','b_pendidikan','status_pemilik', 'sk',
+        'tgl_sk','sk_izin', 'tgl_sk_izin',
 
-        if (!$sekolah) {
+           $user->update();
+
+           $sekolah->nama     = $req->nama;
+           $sekolah->NPSN    = $req->NPSN;
+           $sekolah->status    = $req->status;
+           $sekolah->b_pendidikan    = $req->b_pendidikan;
+           $sekolah->status_pemilik    = $req->status_pemilik;
+           $sekolah->sk    = $req->sk;
+           $sekolah->tgl_sk    = $req->tgl_sk;
+           $sekolah->sk_izin    = $req->sk_izin;
+           $sekolah->tgl_sk_izin    = $req->tgl_sk_izin;
+           $sekolah->update();
+        if (!$user && $sekolah) {
             return $this->returnController("error", "failed find data sekolah");
-        
         }
-        $sekolah->fill($req->all())->save();
+        $merge = (['user' => $user, 'sekolah' => $sekolah]);
 
+        Redis::del("user:all");
+        Redis::set("user:$user_id", $user);
         Redis::del("sekolah:all");
         Redis::set("sekolah:$id", $sekolah);
-        return $this->returnController("ok", $sekolah);
+
+        return $this->returnController("ok", $merge);
     }
 
     public function delete($uuid){
@@ -105,18 +143,26 @@ class SekolahController extends APIController
         if (!$id) {
             return $this->returnController("error", "failed decrypt uuid");
         }
-        $sekolah = sekolah::findOrFail($id);
-        if (!$sekolah) {
+
+        $sekolah = sekolah::find($id);
+        $user = user::find($sekolah->user_id);
+        if (!$user) {
             return $this->returnController("error", "failed find data sekolah");
         }
+
         // Need to check realational
         // If there relation to other data, return error with message, this data has relation to other table(s)
-        $delete = $sekolah->delete();
+        $image_path = "img/user/".$user->foto;  // Value is not URL but directory file path
+        if(File::exists($image_path)) {
+        File::delete($image_path);
+        }
+        $delete = $user->delete();
         if (!$delete) {
             return $this->returnController("error", "failed delete data sekolah");
         }
-        Redis::del("sekolah:all");
-        Redis::del("sekolah:$id");
+
+        Redis::del("user:all");
+        Redis::del("user:$sekolah->user_id");
         return $this->returnController("ok", "success delete data sekolah");
     }
 }
